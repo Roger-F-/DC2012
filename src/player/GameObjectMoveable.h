1773c35e8381aca824ab2387bd1e172e78579116
Po Hsu <haha8887@gmail.com>
2012-04-11 18:43:34 -0700

    updated comments in GOM, not done yet

--- src/player/GameObjectMoveable.h
+++ src/player/GameObjectMoveable.h
@@ -1,48 +1,48 @@
 /*-----------------------------------------------------------------------------
---	CLASS NAME:
---					GameObjectMoveable
---	PARENT CLASS:
---					GameObject
---	CONSTRUCTOR:
---					GameObjectMoveable(int objID, ObjectType type, double degree,
---						double posX, double posY, int playerID, double speed)
---	DESTROCTOR:
+--  CLASS NAME:
+--                  GameObjectMoveable
+--  PARENT CLASS:
+--                  GameObject
+--  CONSTRUCTOR:
+--                  GameObjectMoveable(int objID, ObjectType type, double degree,
+--                      double posX, double posY, int playerID, double speed)
+--  DESTROCTOR:
 --
---	DATA MEMBER:
---					private:
---						int playerID_
---						double speed_;
---	FUNCTIONS:
---					public:
---						int getPlayerID()
---						double getSpeed()
---						void rotatePointAround(Point &pt, const Point &centre_pt, 
---							const double &degree)
---						Point getDirectionalPoint(double distance, double degree)
+--  DATA MEMBER:
+--                  private:
+--                      int playerID_
+--                      double speed_;
+--  FUNCTIONS:
+--                  public:
+--                      int getPlayerID()
+--                      double getSpeed()
+--                      void rotatePointAround(Point &pt, const Point &centre_pt, 
+--                          const double &degree)
+--                      Point getDirectionalPoint(double distance, double degree)
 --
---	DATE:
---					March 9, 2012
---	REVISIONS:
---					March 14, 2012 (Roger)
---						- switched to using double type in Point & Degree & speed
---						- implemented object movement functions
---						- removed accelerate() & decelerate(), they'll be
---						implemented in GOM_Ship class
+--  DATE:
+--                  March 9, 2012
+--  REVISIONS:
+--                  March 14, 2012 (Roger)
+--                      - switched to using double type in Point & Degree & speed
+--                      - implemented object movement functions
+--                      - removed accelerate() & decelerate(), they'll be
+--                      implemented in GOM_Ship class
 --                  April 4, 2012 (Po)
 --                      - modified constructor so that 'type' will be the first
 --                        parameter
 --
---	DESIGNER:
---					Aiko Rose
---					Po Hsu
---					Roger Fan
---					Zach Smoroden
---	PROGRAMMER:
---					Aiko Rose
---					Po Hsu
---					Roger Fan
---					Zach Smoroden
---	NOTES:
+--  DESIGNER:
+--                  Aiko Rose
+--                  Po Hsu
+--                  Roger Fan
+--                  Zach Smoroden
+--  PROGRAMMER:
+--                  Aiko Rose
+--                  Po Hsu
+--                  Roger Fan
+--                  Zach Smoroden
+--  NOTES:
 -----------------------------------------------------------------------------*/
 #ifndef GAMEOBJECTMOVEABLE_H_
 #define GAMEOBJECTMOVEABLE_H_
@@ -56,110 +56,143 @@ const double PI = 4.0 * atan(1.0);
 
 class GameObjectMoveable : public GameObject
 {
-	protected:
-		int 	playerID_;
-		double 	speed_; // speed's unit is pixel
-	public:
-		explicit GameObjectMoveable(ObjectType type, int objID, double degree, double posX, double posY, int playerID, double speed)
-		:GameObject(type, objID, degree, posX, posY), playerID_(playerID), speed_(speed){}
+    protected:
+        int     playerID_;
+        double     speed_; // speed's unit is pixel
+    public:
+/*-----------------------------------------------------------------------------
+--  FUNCTION:
+--                  Constructor
+--  DATE:
+--                  March 9, 2012
+--  REVISIONS:
+--                  March 14, 2012 (Roger)
+--                      - switched to using double type in Point & Degree
+--                  April 4, 2012 (Po)
+--                      - modified constructor so that 'type' will be the first
+--                        parameter
+--  DESIGNER:
+--                  Aiko Rose
+--                  Po Hsu
+--                  Roger Fan
+--                  Zach Smoroden
+--  PROGRAMMER:
+--                  Po Hsu
+--                  Roger Fan
+--  INTERFACE:
+--                  GameObjectMoveable(ObjectType type, int objID, double degree,
+                            double posX, double posY, int playerID, double speed);
+--  RETURNS:
+--                  Constructor
+--  NOTES:
+--                  type     Object type used by the graphics team
+--                  objID    Unique object ID
+--                  degree   Initial orientation
+--                  posX     X-coordinate
+--                  posY     Y-coordinate
+--                  playerID Owner ID
+--                  speed    Initial speed
+-----------------------------------------------------------------------------*/
+        explicit GameObjectMoveable(ObjectType type, int objID, double degree, double posX, double posY, int playerID, double speed)
+        :GameObject(type, objID, degree, posX, posY), playerID_(playerID), speed_(speed){}
 
 /*-----------------------------------------------------------------------------
---	FUNCTION:
---				getPlayerID
---	DATE:
---				March 9, 2012
---	REVISIONS:
+--  FUNCTION:
+--              getPlayerID
+--  DATE:
+--              March 9, 2012
+--  REVISIONS:
 --
---	DESIGNER:
---				Roger Fan
---	PROGRAMMER:
---				Roger Fan
---	INTERFACE:
---				int getPlayerID() const
---	RETURNS:
---				Returns playerID_ variable.
---	NOTES:
+--  DESIGNER:
+--              Roger Fan
+--  PROGRAMMER:
+--              Roger Fan
+--  INTERFACE:
+--              int getPlayerID() const
+--  RETURNS:
+--              Returns playerID_ variable.
+--  NOTES:
 -----------------------------------------------------------------------------*/
-		int getPlayerID() const;
-		
+        int getPlayerID() const;
+        
 /*-----------------------------------------------------------------------------
---	FUNCTION:
---				getSpeed
---	DATE:
---				March 9, 2012
---	REVISIONS:
+--  FUNCTION:
+--              getSpeed
+--  DATE:
+--              March 9, 2012
+--  REVISIONS:
 --
---	DESIGNER:
---				Roger Fan
---	PROGRAMMER:
---				Roger Fan
---	INTERFACE:
---				double getSpeed() const
---	RETURNS:
---				Returns speed_ variable.
---	NOTES:
+--  DESIGNER:
+--              Roger Fan
+--  PROGRAMMER:
+--              Roger Fan
+--  INTERFACE:
+--              double getSpeed() const
+--  RETURNS:
+--              Returns speed_ variable.
+--  NOTES:
 -----------------------------------------------------------------------------*/
-		double getSpeed() const;
-		
+        double getSpeed() const;
+        
 /*-----------------------------------------------------------------------------
---	FUNCTION:
---				rotatePointAround
---	DATE:
---				March 9, 2012
---	REVISIONS:
+--  FUNCTION:
+--              rotatePointAround
+--  DATE:
+--              March 9, 2012
+--  REVISIONS:
 --
---	DESIGNER:
---				Roger Fan
---	PROGRAMMER:
---				Roger Fan
---	INTERFACE:
---				void rotatePointAround(Point &pt, const Point &centre_pt, 
---					const double &degree)
---	RETURNS:
---				void
---	NOTES:
---				This function operates on parameter pt. It updates the (x, y)
---				coordinates of Point pt to the position after it rotates around
---				Point centre_pt for the number of degree specified.
+--  DESIGNER:
+--              Roger Fan
+--  PROGRAMMER:
+--              Roger Fan
+--  INTERFACE:
+--              void rotatePointAround(Point &pt, const Point &centre_pt, 
+--                  const double &degree)
+--  RETURNS:
+--              void
+--  NOTES:
+--              This function operates on parameter pt. It updates the (x, y)
+--              coordinates of Point pt to the position after it rotates around
+--              Point centre_pt for the number of degree specified.
 -----------------------------------------------------------------------------*/
-		void rotatePointAround(Point &pt, const Point &centre_pt, const double &degree);
-		
+        void rotatePointAround(Point &pt, const Point &centre_pt, const double &degree);
+        
 /*-----------------------------------------------------------------------------
---	FUNCTION:
---				getDirectionalPoint
---	DATE:
---				March 9, 2012
---	REVISIONS:
+--  FUNCTION:
+--              getDirectionalPoint
+--  DATE:
+--              March 9, 2012
+--  REVISIONS:
 --
---	DESIGNER:
---				Roger Fan
---	PROGRAMMER:
---				Roger Fan
---	INTERFACE:
---				Point getDirectionalPoint(double distance, double degree)
---	RETURNS:
---				Returns a Point object.
---	NOTES:
---				Example of this function's usage:
---				
---				Point a(2, 2);
---				Point temp = getDirectionalPoint(10, 30);
---				a += temp;
+--  DESIGNER:
+--              Roger Fan
+--  PROGRAMMER:
+--              Roger Fan
+--  INTERFACE:
+--              Point getDirectionalPoint(double distance, double degree)
+--  RETURNS:
+--              Returns a Point object.
+--  NOTES:
+--              Example of this function's usage:
+--              
+--              Point a(2, 2);
+--              Point temp = getDirectionalPoint(10, 30);
+--              a += temp;
 --
---				Point a now has moved 10 units into 30 degree direction.
+--                Point a now has moved 10 units into 30 degree direction.
 -----------------------------------------------------------------------------*/
-		Point getDirectionalPoint(double distance, double degree);
+        Point getDirectionalPoint(double distance, double degree);
         
         // rotate hitbox
-		void rotateHitbox(double degree);
-		// move hitbox
-		void moveHitbox(double x, double y);
-		
-		virtual void update(const std::string &str) = 0;
-		virtual std::string toString() const = 0;
+        void rotateHitbox(double degree);
+        // move hitbox
+        void moveHitbox(double x, double y);
+        
+        virtual void update(const std::string &str) = 0;
+        virtual std::string toString() const = 0;
 
-		// for testing purposes
-		virtual void print(std::ostream& os) const;
+        // for testing purposes
+        virtual void print(std::ostream& os) const;
 };
 
 #endif
